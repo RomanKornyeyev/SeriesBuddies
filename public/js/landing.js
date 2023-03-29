b6cee426ff4b428f581d18c78ca947d2b887db9a
SrGalleto <romankr1997@gmail.com>
2023-03-30 01:21:31 +0200

    landing terminada

---
+++ public/js/landing.js
@@ -0,0 +1,14 @@
+// obtén los elementos
+const elemento1 = document.getElementById('row-top');
+const elemento2 = document.getElementById('row-bottom');
+
+// agrega un manejador de eventos para el movimiento del ratón en el la ventana
+window.addEventListener('mousemove', function(event) {
+  //pos ratón en el cliente
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;  
+
+  // actualiza la posición del contenido en relación a la posición del ratón
+  elemento1.style.transform = `translate(-${mouseX * 0.05}px, -${mouseY * 0.05}px)`;
+  elemento2.style.transform = `translate(${mouseX * 0.05}px, ${mouseY * 0.05}px)`;
+});
