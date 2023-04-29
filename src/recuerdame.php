a4cb6b91f18cc5ace05f5cb1a97d9fe95b83376a
roman <romankr1997@gmail.com>
2023-04-29 02:00:08 +0200

    arreglado error de recuerdame, no comprobaba la fecha de expiración

--- src/recuerdame.php
+++ src/recuerdame.php
@@ -7,27 +7,38 @@
             //si la tiene, cogemos el valor y comprobamos a ver si coincide con alguno de la base de datos
             $tkn = $_COOKIE['recuerdame'];
             $db->ejecuta(
-                "SELECT * FROM usuarios WHERE id=(SELECT id_usuario FROM tokens WHERE valor=?);",
+                "SELECT * FROM tokens WHERE valor=?;",
                 $tkn
             );
             $consulta = $db->obtenElDato();
 
-            //si la consulta devuelve un user, es que la cookie es buena, y hacemos $_SESSION de nombre
-            if ($consulta != "") {
-                //nos traemos todos los datos del user
-                $_SESSION['id'] = $consulta['id'];
-                $_SESSION['nombre'] = $consulta['nombre'];
-                $_SESSION['contra'] = $consulta['contra'];
-                $_SESSION['img'] = $consulta['img'];
-                $_SESSION['correo'] = $consulta['correo'];
-                $_SESSION['privilegios'] = $consulta['privilegios'];
-
+            //si el token es válido y no está expirado
+            if ($consulta != "" && $consulta['expiracion'] > date('Y-m-d H:i:s')) {
                 
-                //alargamos la vida del token (7 días)
+                //buscamos el user
                 $db->ejecuta(
-                    "UPDATE tokens SET expiracion=(NOW() + INTERVAL 7 DAY) WHERE valor=?",
+                    "SELECT * FROM usuarios WHERE id=(SELECT id_usuario FROM tokens WHERE valor=?);",
                     $tkn
                 );
+                $consulta = $db->obtenElDato();
+
+                //si la consulta devuelve un user, es que la cookie es buena, y hacemos $_SESSION de nombre
+                if ($consulta != ""){
+                    //nos traemos todos los datos del user
+                    $_SESSION['id'] = $consulta['id'];
+                    $_SESSION['nombre'] = $consulta['nombre'];
+                    $_SESSION['contra'] = $consulta['contra'];
+                    $_SESSION['img'] = $consulta['img'];
+                    $_SESSION['correo'] = $consulta['correo'];
+                    $_SESSION['privilegios'] = $consulta['privilegios'];
+
+
+                    //alargamos la vida del token (7 días)
+                    $db->ejecuta(
+                        "UPDATE tokens SET expiracion=(NOW() + INTERVAL 7 DAY) WHERE valor=?",
+                        $tkn
+                    );
+                }
             }
         }
     }
