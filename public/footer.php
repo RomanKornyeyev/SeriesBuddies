148a18a32d5fa0f66d2f31bb6da8ea3d8363bb67
roman <romankr1997@gmail.com>
2023-05-16 00:02:11 +0200

    footer actualizado, añadiendo el nuevo logo

--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,9 @@
 <footer class="footer">
     <div class="footer__content limit-width">
-        <a href="index.php"><h1 class="logo">SeriesBuddies</h1></a>
+        <a href="index.php">
+            <!-- <h1 class="logo">SeriesBuddies</h1> -->
+            <img src="upload/logos/logo-principal.png" alt="Logo SeriesBuddies" class="logo-footer">
+        </a>
         <nav class="nav nav--footer">
             <a href="contact.php" class="nav__link--footer">Contacto</a>
             <a href="#" class="nav__link--footer">Términos y condiciones</a>
