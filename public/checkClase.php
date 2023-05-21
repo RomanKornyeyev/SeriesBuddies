7c07caf6501e11f9958cf3041b398a8659f437c1
Anabelix <anabelpedrajas@hotmail.com>
2023-05-21 17:50:53 +0200

    Actualizando checkclase.php

--- public/checkClase.php
+++ public/checkClase.php
@@ -1,17 +1,152 @@
 <?php
 require_once("../src/init.php");
-use form\claseMain\TMDB;
-
+use clases\api_tmdb\TMDB;
 $tmdb=new TMDB();
+
+//---------------------------------PARA LAS FOTOS DE LAS SERIES POR ID--------------------------------
+/*
+$idSerie = 112888;
+$url=$tmdb->urlSerieImg($idSerie);
+$fotos = $tmdb->peticionHTTP($url)['backdrops'];
+echo '<pre>';
+print_r($fotos);
+echo '</pre>';
+
+foreach ($fotos as $key => $value) {
+    $imagenes[$key]['file_path'] = 'https://image.tmdb.org/t/p/w1280'.$fotos[$key]['file_path'];
+}
+echo '<pre>';
+print_r($imagenes);
+echo '</pre>';
+
+foreach ($fotos as $key => $value) {
+    echo "<img src=".$imagenes[$key]['file_path'].">";
+}
+*/
+
+
+
+//--------------------------------PARA LA PAGINACION DE LOS COMENTARIOS--------------------------------
+/*
+$idSerie = 112888;
+$paginaActual = 2;
+$registrosPagina = 5;
+
+$registroInicial = ($paginaActual-1)*$registrosPagina;
+echo "Registro inicial: $registroInicial<br>";
+
+$comentarios = $db->ejecuta("SELECT u.nombre, id_post, contenido, DATE_FORMAT(fecha, '%d %b %Y', 'es-ES') as fecha, img FROM respuestas r INNER JOIN usuarios u ON u.id=r.id_usuario WHERE id_post=? ORDER BY fecha LIMIT ?, ?;", $idSerie, $registroInicial, $registrosPagina);
+$comentarios = $db->obtenDatos();
+$comentarios = $db->getRespuestasSerie($db, $idSerie, $registroInicial);
+
+echo '<pre>';
+print_r($comentarios);
+echo '</pre>';
+
+$totalRegistros = $db->ejecuta('SELECT COUNT(*) FROM respuestas where id_post = ?;', $idSerie);
+$totalRegistros = $db->obtenElDato()['COUNT(*)'];
+echo "Total de los registros: $totalRegistros<br>";
+
+$totalPaginas   = ceil($totalRegistros / $registrosPagina);
+echo "Total paginas: $totalPaginas<br>";
+*/
+
+
+//--------------------------------PARA LA PAGINA BUDDIES--------------------------------
+/*
+$listadoBuddies     = $db->ejecuta ("SELECT 
+                                    DISTINCT(id_post), u.id, nombre, correo, img 
+                                    FROM respuestas r 
+                                    INNER JOIN usuarios u ON u.id=r.id_usuario 
+                                    WHERE id_post=? 
+                                    LIMIT 10;", $idSerie);
+$listadoBuddies     = $db->obtenDatos();
+
+$totalRespuestas    = $db->ejecuta("SELECT id_usuario, COUNT(id_usuario) FROM respuestas GROUP BY id_usuario;");
+$totalRespuestas    = $db->obtenDatos();
+
+$totalSeries        = $db->ejecuta("WITH t AS (
+                                                SELECT id_post, id_usuario 
+                                                FROM respuestas 
+                                                GROUP BY id_usuario, id_post 
+                                                ORDER BY id_usuario, id_post) 
+                                    SELECT count(id_post)
+                                    FROM t GROUP BY id_usuario;");
+$totalSeries        = $db->obtenDatos();
+
+foreach ($listadoBuddies as $key => $value) {
+    if ($listadoBuddies[$key]['id'] == $totalRespuestas[$key]['id_usuario']) {
+        $listadoBuddies[$key]['total_series']       = $totalSeries[$key]['COUNT(id_post)'];
+        $listadoBuddies[$key]['total_respuestas']   = $totalRespuestas[$key]['COUNT(id_usuario)'];
+        $listadoBuddies[$key]['total_buddies']      = random_int(1, 20);
+        $listadoBuddies[$key]['total_chips']        = random_int(1, 20);
+    }
+}
+
+echo '<pre>';
+print_r($listadoBuddies);
+echo '</pre>';
+*/
+
+
+
+/*
+$idUsuario = 1;
+$infoBuddie = $db->ejecuta("SELECT id, nombre, img, correo, descripcion FROM usuarios WHERE id=?;", $idUsuario);
+$infoBuddie = $db->obtenDatos()[0];
+
+
+
+$series = $db->ejecuta("SELECT DISTINCT(id_post) FROM respuestas WHERE id_usuario=?;", $idUsuario);
+$series = $db->obtenDatos();
+// array_walk($series, function ($key, $value) {
+//     foreach ($key as $key => $value) {
+//         echo $value." ";
+//     }
+// });
+foreach ($series as $key => $value) {
+    $series[$key] = $series[$key]['id_post'];
+}
+array_push($series[$key]);
+echo '<pre>';
+print_r($infoBuddie);
+echo '</pre>';
+
+echo '<pre>';
+print_r($series);
+echo '</pre>';
+*/
+
+// WITH series AS (SELECT DISTINCT(id_post)'id_serie', id_usuario FROM respuestas WHERE id_usuario=1) SELECT u.id, nombre, img, correo, descripcion, id_serie
+// FROM series s, usuarios u WHERE s.id_usuario = u.id;
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------
+
+// $serieID = 112888;
+// $consulta = $db->ejecuta('SELECT u.nombre, id_post, contenido, fecha, img FROM respuestas r INNER JOIN usuarios u ON u.id=r.id_usuario WHERE id_post=? ORDER BY fecha LIMIT 5;', $serieID);
+// $consulta = $db->obtenDatos();
+
+// $totalPaginas = $db->ejecuta('SELECT COUNT(*) FROM respuestas where id_post = ?;', $serieID);
+// $totalPaginas = $db->obtenElDato()['COUNT(*)'];
+// print_r($totalPaginas);
+
+//-----------------------------------------------------------------------------------------------
+
+
+
 // echo $tmdb->getAPIUrl().'<br>';
 // echo $tmdb->getToken().'<br>';
 // echo ($tmdb->getAdultFilter())? 'true<br>':'false<br>';
 // echo $tmdb->getLang().'<br>';
 
-// $url = $tmdb->urlSerie('true beauty');
+//$url = $tmdb->urlListadoGeneros();
 // $url = $tmdb->urlSeriesGeneros(18);
-$url = $tmdb->urlListadoGeneros();
-$response = $tmdb->peticionHTTP($url)['genres'];
+// $url = $tmdb->urlSerie('true beauty');
+// $response = $tmdb->peticionHTTP($url);
 // echo '<pre>';
 // print_r($response);
 // echo '</pre>';
@@ -23,73 +158,43 @@ $response = $tmdb->peticionHTTP($url)['genres'];
 //informacion de una serie
 
 // $url = $tmdb->urlListadoGeneros();
+// $url = $tmdb->urlSeriesGeneros(99, 10);
 // $listadoGeneros = $tmdb->peticionHTTP($url);
-// // echo '<pre>';
-// // print_r($listadoGeneros);
-// // echo '</pre>';
+// echo '<pre>';
+// print_r($listadoGeneros);
+// echo '</pre>';
 
 // $serie=$tmdb->getSerie('mr. queen');
 // // echo '<pre>';
 // // var_dump($serie);
 // // echo '</pre>';
 
-// $url = $tmdb->urlSeriesGeneros(18);
 // //echo $url;
-// $response = $tmdb->getSeriesGenero($url);
-// // echo '<pre>';
-// // var_dump($response);
-// // echo '</pre>';
+// $registrosPagina = 20;
+// $totalPaginas = 500;
 
+// if (isset($_GET['pagina'])) {
+//     $paginaActual = $_GET['pagina'];
+// } else {
+//     $paginaActual = 1;
+// }
 
-// $series=$tmdb->getSeriesNombre('kingdom');
+// $response = $tmdb->getSeriesGenero(18, 1);
 // echo '<pre>';
-// print_r($series);
+// var_dump($response);
 // echo '</pre>';
+// for ($i=1; $i <= $totalPaginas; $i++) {
+//     echo '<a href="checkClase.php?pagina='.$i.'">'.$i.'</a> ';
+// }
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$serie['serieTitle']?></title>
-</head>
-<body>
-<h1 class="title title--l text-align-center">GÉNEROS</h1>
-    <div class="main__content">
-        
-        <?php foreach ($response as $key => $value) {
-            $url = $tmdb->urlSeriesGeneros($response[$key]['id']); 
-            $resultados = $tmdb->peticionHTTP($url);
-            ?>
-            <div class="box">
-                <a href="./series.php?id=<?=$response[$key]['id']?>" class="box-body-wrapper">
-                    <div class="box__body box__body--gender">
-                        <div class="box-body__info">
-                            <h2 class="title title-gender"><?=$response[$key]['name']?></h2>
-                            <p class="text-white"><?=$resultados['total_results']?> resultados &gt;</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        <?php } ?>
+//$series=$tmdb->getSeriesNombre('simpson');
+$urlSerie =$tmdb->urlSerieNombre('simpson');
+$series=$tmdb->peticionHTTP($urlSerie)['results'];
+echo '<pre>';
+print_r($series);
+echo '</pre>';
 
-    </div>
-    <script>
-        const options = {
-            method: 'GET',
-            headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZWQyODFjMmE4YzU2NmIxMTM1OGYxMzA3Yzc4ZWUyMSIsInN1YiI6IjY0NGQzNzliMmQzNzIxMjg4NjAxMDIwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dF1hwzUEcQB-Ad-r7vwD3bgCj4W3vctULyT1bHGxQoE'
-            }
-        };
-        
-        fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=2&sort_by=popularity.desc&with_genres=18', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
-    </script>
-</body>
-
-</html>
+// $response=$tmdb->getSerieID(112888);
+// echo '<pre>';
+// print_r($response);
+// echo '</pre>';
