bf57c0bb37f54fe956735885780244c9ac1f20ae
SrGalleto <romankr1997@gmail.com>
2023-06-10 00:00:11 +0200

    hotfix pagina anterior

--- src/paginaAnterior.php
+++ src/paginaAnterior.php
@@ -1,14 +1,14 @@
 <?php
 
     //para que si venimos de una privada, nos mantenga la página anterior visitada
-    $paginaAnterior = (isset($_SESSION['anterior'])) ? $_SESSION['anterior'] : "/public/index.php";
+    $paginaAnterior = (isset($_SESSION['anterior'])) ? $_SESSION['anterior'] : "./index.php";
 
     //mientras no sea login, ni register, etc. ni ningún handler, no actualices $session de anterior
     if (
         !preg_match('/login/i', $_SERVER["REQUEST_URI"])
-        && $_SERVER["REQUEST_URI"] != "/public/register.php"
-        && $_SERVER["REQUEST_URI"] != "/public/verify.php"
-        && $_SERVER["REQUEST_URI"] != "/public/recovery.php"
+        && !preg_match('/register/i', $_SERVER["REQUEST_URI"])
+        && !preg_match('/verify/i', $_SERVER["REQUEST_URI"])
+        && !preg_match('/recovery/i', $_SERVER["REQUEST_URI"])
         && !preg_match('/handler/i', $_SERVER["REQUEST_URI"])
     ){
         $_SESSION["anterior"] = $_SERVER["REQUEST_URI"];
